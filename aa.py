a136c81968cf1786c73646a5f5faae2ab2becc83
axtrais <90709422+axtrais@users.noreply.github.com>
2021-12-09 15:21:26 +0100

    Add files via upload

---
+++ aa.py
@@ -0,0 +1,7 @@
+h=int(input("entrée la hauteur :"))
+l=int(input("entrée la longueur :"))
+for i in range(h):
+    for j in range(l):
+        print("#",end="")
+    print("")
+   
---
+++ ex 112.py
@@ -0,0 +1,12 @@
+a=0
+b=0
+for i in range (10):
+    a=a+1
+    print("Table de",a,":")
+    b=0
+    for j in range(10):
+        b=b+1
+        c=0
+        c=a*b
+        print(a,"*",b,"=",c)
+        
